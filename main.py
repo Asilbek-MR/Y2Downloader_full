b85c7fca7cafe0e61f3fc99d4c049fe5baa3fb0a
asilbek9919 <105420765+asilbek9919@users.noreply.github.com>
2023-03-09 16:56:26 +0500

    add middleware

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Request,Form
+from fastapi import FastAPI, Request, Form
+from fastapi.middleware.cors import CORSMiddleware 
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
@@ -8,6 +9,16 @@ from starlette.exceptions import HTTPException as StarletteHTTPException
 import os
 from pathlib import Path
 app = FastAPI()
+
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+) 
 
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
